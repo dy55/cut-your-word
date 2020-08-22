ee5193f4d4395c315ea83f0bebda0ec7ea4bc098
dy_55 <minidy20001001@outlook.com>
2020-08-22 16:51:51 +0800

    Add concat logic

---
+++ csv_concat.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+# Merge two rows
+#
+# @param row1 [Array<String>]
+# @param row2 [Array<String>]
+# @return [Array<String>]
+def merge(row1, row2)
+  if row1.length < row2.length
+    source_arr = row1
+    new_row = row2.dup
+  else
+    source_arr = row2
+    new_row = row1.dup
+  end
+
+  source_arr.each_index do |index|
+    new_row[index] += source_arr[index] unless source_arr[index].nil?
+  end
+
+  new_row
+end
+
+path = './raw/dict-raw2.csv'
+
+rows = CSV.read(path)
+
+new_table = []
+
+print 'Start concatenating...'
+
+replaced_hash = {
+  "Latin, Greek": /(LatinGreek|GreekLatin)/,
+  "Latin?, Greek?": /(Latin\??Greek|Greek\??Latin)\?/
+}
+
+rows.each do |row|
+  if row[0].nil? || row[1].nil? || row[3].nil?
+    new_table[-1] = merge(new_table[-1], row)
+
+    replaced_hash.each do |key, value|
+      if value.match? new_table[-1][2]
+        new_table[-1][2] = key
+        break
+      end
+    end
+  else
+    new_table.append row
+  end
+end
+
+CSV.open(path, 'w') do |csv|
+  new_table.each { |row| csv << row }
+end
+
+puts ' done'
--- wordpart/word.rb
+++ wordpart/word.rb
@@ -1,9 +1,30 @@
 # frozen_string_literal: true
 
+# Word class
 class Word
   attr_reader :content
 
-  def include?
-    @content.include?
+  def initialize(word_string)
+    @content = word_string
+  end
+
+  # Whether this word includes the input string
+  #
+  # @param input_string [String] Input string
+  #
+  # @return [Boolean] If it is included
+  def include?(input_string)
+    @content.include? input_string
+  end
+
+  # Cut the word into parts according to dictionaries
+  #
+  # @param root_dict [hash<String, String>] Root dictionary
+  # @param prefix_dict [hash<String, String>] Prefix dictionary
+  # @param suffix_dict [hash<String, String>] Suffix dictionary
+  #
+  # @return [Array<WordPart>] Word parts
+  def cut(root_dict, prefix_dict, suffix_dict)
+    # TODO
   end
 end
